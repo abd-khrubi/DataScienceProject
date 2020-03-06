91ca910dd06800754ea0673bb6a39dd548051cfb
abd_khrubi <abd_khrubi@cs.huji.ac.il>
2020-03-06 19:55:51 +0200

    Updates

---
+++ precache-manifest.47343c4b86e3b857249f917d45b4162d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d55473ca5cb24caa48c9b5ced44b2c",
+    "url": "/NodeTest/index.html"
+  },
+  {
+    "revision": "c59fea4f1e88ad2878f3",
+    "url": "/NodeTest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ebd0ae3a128259ad54d8",
+    "url": "/NodeTest/static/js/2.3f4eaecd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/NodeTest/static/js/2.3f4eaecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c59fea4f1e88ad2878f3",
+    "url": "/NodeTest/static/js/main.7ae7df7a.chunk.js"
+  },
+  {
+    "revision": "258c75c16cc87fbb3910",
+    "url": "/NodeTest/static/js/runtime-main.c223b1c4.js"
+  }
+]);
